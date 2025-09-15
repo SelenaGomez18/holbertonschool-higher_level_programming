ee813c06c368916af048182ad99a2576c656628a
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-09-14 23:50:26 -0500

    Add a programthat prints the ASCII alphabet, in reverse order

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):  # Rango de z (122) a A (65)
-    if (122 - i) % 2 == 0:  # Alterna entre minúsculas y mayúsculas
-        print(chr(i), end="")
-    else:
-        print(chr(i - 32), end="")
+print("".join("{}".format(chr(i) if (122 - i) % 2 == 0 else chr(i - 32)) for i in range(122, 96, -1)), end="")
