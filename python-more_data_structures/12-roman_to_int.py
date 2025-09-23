8b46062bd987ae68c9edd313bdaff0b75e7997ba
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-09-23 01:48:14 -0500

    Add a function that converts a Roman numeral to an integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
+    roman_val = dict([('I', 1), ('V', 5), ('X', 10), ('L', 50), ('C', 100), ('D', 500), ('M', 1000)])
+    count = 0
+    for x in range(len(roman_string)):
+        current = roman_val[roman_string[x]]
+        if x + 1 < len(roman_string):
+            next_val = roman_val[roman_string[x + 1]]
+        else:
+            next_val = 0
+        if current < next_val:
+            count -= current
+        else:
+            count += current
+    return count
--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key, None); return a_dictionary
-
+    if key in a_dictionary:
+        del a_dictionary[key]
+    else:
+        return a_dictionary
+    return a_dictionary
