c3c8ee3d2949ad2558072059eebd1a3312231f3f
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-09-28 22:53:00 -0500

    Add correction tests for max integer

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,6 +5,15 @@ Print text with 2 new lines after '.', '?' and ':'.
 
 
 def text_indentation(text):
+    """
+    Print a text with 2 new lines after '.', '?' and ':'.
+
+    Args:
+        text (str): The text to process.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
