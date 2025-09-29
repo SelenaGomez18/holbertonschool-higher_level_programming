12a3d5c5c6c613590469e4fd6c4b9e239d134584
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-09-28 20:59:24 -0500

    Add tests of matrix divided

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
 """
 2-matrix_divided module
-Divides all elements of a matrix by a number.
+This module contains a function to divide all elements of a matrix.
 """
 
 
 def matrix_divided(matrix, div):
     """
-    Divides all elements of a matrix by div, rounding to 2 decimal places.
+    Divides all elements of a matrix by div.
 
     Args:
-        matrix (list): A list of lists of integers or floats.
-        div (int/float): The number to divide by.
+        matrix: list of lists of integers or floats
+        div: number (integer or float)
 
     Returns:
-        list: A new matrix with all elements divided by div.
+        New matrix with all elements divided by div, rounded to 2 decimal places
 
     Raises:
-        TypeError: If matrix is not a list of lists of integers/floats,
-                   or if div is not a number.
-        ZeroDivisionError: If div is zero.
+        TypeError: if matrix is not a list of lists of ints/floats,
+                   or if rows are not the same size,
+                   or if div is not a number
+        ZeroDivisionError: if div is zero
     """
+    if (
+        not isinstance(matrix, list) or
+        not all(isinstance(row, list) for row in matrix) or
+        not all(isinstance(num, (int, float)) for row in matrix for num in row)
+    ):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    if not all(isinstance(num, (int, float)) for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    row_lengths = [len(row) for row in matrix]
-    if not all(length == row_lengths[0] for length in row_lengths):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
