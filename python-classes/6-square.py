be89532245e728a6da68e252a868c9ba5a912115
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-10-05 20:01:20 -0500

    Add the instance def my_print for print a square with '#'

---
+++ python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Module for Square class.
+"""
+
+
+class Square:
+    """
+    Defines a square.
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple)
+        or len(value) != 2
+        or any(not isinstance(num, int)or num < 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square (size * size).
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end="")
+            for _ in range(self.__size):
+                print('#' * self.size + " " * self.position[0])
