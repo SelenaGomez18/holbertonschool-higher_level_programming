8dcbaa943464840cc023771decbf11ffd61dc287
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-10-18 14:05:44 -0500

    Write a class BaseGeometry, def integer_validator(self, name, value): and errors, I created too a file 7-base_geometry.txt for make tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,6 +12,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validate that value is an integer greater than 0."""
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
