8c84cefa11b52eaa652663dd29eb41751e0c5dfd
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-10-18 18:02:17 -0500

    Write a class Square that inherits from Rectangle

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-
+"""Defines a Square class that inherits from Rectangle."""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Represents a square, inherits from Rectangle."""
+
     def __init__(self, size):
+        """Initialize a square with a given size."""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
+        """Return the area of the square."""
         return self.__size ** 2
 
     def __str__(self):
+        """Return string representation: [Square] <width>/<height>."""
         return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
