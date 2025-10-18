ddb07ddd0648d6a3649e4d80f41bc93aea4d70ce
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-10-17 21:55:50 -0500

    Write an empty class BaseGeometry

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,6 +4,7 @@ Module to check if an object is an instance of a class
 or inherits from it.
 """
 
+
 def is_kind_of_class(obj, a_class):
     """Return True if obj is instance of or inherits from a_class."""
     return isinstance(obj, a_class)
--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+Module that defines a base class for geometry objects.
+"""
 
 
 class BaseGeometry:
+    """Base class for geometric shapes."""
+
     def area(self):
+        """Raise an exception because area() is not implemented."""
         raise Exception("area() is not implemented")
