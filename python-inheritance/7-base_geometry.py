3edbd7d5d539fe0dd489ad063191efd71c947798
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-10-18 14:20:47 -0500

    Write a class BaseGeometry, def integer_validator(self, name, value): and errors, I created too a file 7-base_geometry.txt for make tests

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,7 +13,8 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validate that value is an integer greater than 0."""
-        if type(value) is not int:
+        if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
