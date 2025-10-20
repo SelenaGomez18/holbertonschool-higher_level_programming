55ec196e84a380eecb64f9bcfde7e467b7cd4b59
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-10-19 19:36:53 -0500

    Write a class Square that inherits from Rectangle

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
+"""
+Module that provides a function to safely add attributes to objects.
+"""
+
+
 def add_attribute(obj, attr_name, attr_value):
+    """
+    Adds a new attribute to an object if possible.
+
+    Args:
+        obj: The object to add the attribute to.
+        attr_name (str): The name of the attribute.
+        attr_value: The value to assign to the attribute.
+
+    Raises:
+        TypeError: If the object can't have new attributes.
+    """
     if hasattr(obj, "__dict__"):
         setattr(obj, attr_name, attr_value)
     else:
         raise TypeError("can't add new attribute")
-
