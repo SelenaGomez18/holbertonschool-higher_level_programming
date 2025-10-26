ec00ae86ea44375cefeb4a59025fd2d4c55c7a48
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-10-26 18:11:41 -0500

    Add Readme and firts file of abstract clases, here I created a my first class abstract with one method abstract and two class that inherits from Animal(abstract class)

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Module defining abstract Shape class, Circle and Rectangle, and a shape_info function."""
+
+
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    """Abstract base class for geometric shapes."""
+
+    @abstractmethod
+    def area(self):
+        """Calculate the area of the shape."""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Calculate the perimeter of the shape."""
+        pass
+
+class Circle(Shape):
+    """Circle shape class."""
+
+    def __init__(self, radius):
+        """Initialize circle with a radius."""
+        self.radius = radius
+
+    def area(self):
+        """Return the area of the circle."""
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        """Return the perimeter (circumference) of the circle."""
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    """Rectangle shape class."""
+
+    def __init__(self, width, heigth):
+        """Initialize rectangle with width and height."""
+        self.width = width
+        self.heigth = heigth
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.width * self.heigth
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle."""
+        return 2 * (self.width + self.heigth)
