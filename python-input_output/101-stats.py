a38dcd146dd6d3ab9a5a20082f10d196fdc4c740
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-11-02 17:35:00 -0500

    Write a script that reads stdin line by line and computes metrics

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Reads stdin line by line and computes metrics:
+"""
+Reads stdin line by line and computes metrics:
 total file size and count of status codes.
-Prints stats every 10 lines and at keyboard interruption."""
-
-
+Prints stats every 10 lines and at keyboard interruption.
+"""
 import sys
-
 
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 total_size = 0
@@ -18,23 +17,24 @@ def print_stats():
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
 
-try:
-    for line in sys.stdin:
-        parts = line.split()
-        if len(parts) >= 7:
-            try:
-                status = int(parts[-2])
-                size = int(parts[-1])
-            except (ValueError, IndexError):
-                continue
-            total_size += size
-            if status in status_codes:
-                status_codes[status] += 1
-        count += 1
-        if count % 10 == 0:
-            print_stats()
-except KeyboardInterrupt:
-    print_stats()
-    raise
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            parts = line.split()
+            if len(parts) >= 7:
+                try:
+                    status = int(parts[-2])
+                    size = int(parts[-1])
+                except (ValueError, IndexError):
+                    continue
+                total_size += size
+                if status in status_codes:
+                    status_codes[status] += 1
+            count += 1
+            if count % 10 == 0:
+                print_stats()
+    except KeyboardInterrupt:
+        print_stats()
+        raise
 
-print_stats()
+    print_stats()
