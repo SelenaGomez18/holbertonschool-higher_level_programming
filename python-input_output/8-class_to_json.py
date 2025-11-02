69c46e826bde79b87a394933429716006086becc
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-11-02 16:33:59 -0500

    Add function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,7 +6,8 @@
 def class_to_json(obj):
     """
     Returns a dictionary representation of a class instance `obj`
-    containing only attributes that are serializable (list, dict, str, int, bool)
+    containing only attributes that are serializable
+    (list, dict, str, int, bool)
     """
     return {key: value for key, value in vars(obj).items()
             if isinstance(value, (list, dict, str, int, bool))}
