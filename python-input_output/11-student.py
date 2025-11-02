3fc4bb23d2e5b05ceee460bb65c72307616efb43
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-11-02 17:13:43 -0500

    Add Public method def reload_from_json(self, json)

---
+++ python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module to convert class objects to JSON-serializable dict"""
+
+
+class Student:
+    """Defines a student."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance.
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance."""
+        if isinstance(attrs, list) and all(isinstance(item, str)
+                                           for item in attrs):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        based on a dictionary (json).
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
