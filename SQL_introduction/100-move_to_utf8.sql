a83c3c66cccddae7a9521b4233a373c6287f68cc
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-01-04 17:51:03 +0000

    Add a script that list all rows  of the table

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,20 +1,13 @@
--- Convert the database, table, and column to UTF8 (utf8mb4) with collation utf8mb4_unicode_ci
-
--- Select the database first
+-- Select the database to work on
 USE hbtn_0c_0;
 
--- Convert the database hbtn_0c_0
+-- Convert the database hbtn_0c_0 to UTF8 (utf8mb4)
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
--- Convert the table first_table
+-- Convert the table first_table to UTF8 (utf8mb4)
+-- All columns will inherit the table charset, including 'name'
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- Convert the column 'name' in first_table
-ALTER TABLE first_table
-MODIFY name VARCHAR(256) 
-CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
