a8a0fe98cefe3c54edcaa341d7cc385055bd37a4
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-01-11 16:34:09 -0500

    Add a script that creates the table unique_id on your MySQL server.

---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- This script lists all shows that are not linked to the genre Comedy
+SELECT
+    title
+FROM tv_shows
+WHERE id NOT IN (
+    SELECT show_id
+    FROM tv_show_genres
+    JOIN tv_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
