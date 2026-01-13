fa19e86d71b01be77c96f55914ce49a0673b9773
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-01-13 15:20:52 -0500

    Add a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,25 +3,29 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    user = sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
+    db_name = sys.argv[3]
     state_name = sys.argv[4]
 
+    # Conexión a la base de datos
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=user,
+        user=username,
         passwd=password,
-        db=database
+        db=db_name
     )
 
     cursor = db.cursor()
 
+    # Consulta SQL usando format (como pide el ejercicio)
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
     cursor.execute(query)
 
-    for row in cursor.fetchall():
+    # Mostrar resultados
+    results = cursor.fetchall()
+    for row in results:
         print(row)
 
     cursor.close()
