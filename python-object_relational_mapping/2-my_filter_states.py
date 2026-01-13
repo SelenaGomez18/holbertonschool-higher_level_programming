62eac59ee83623eecffd1612417172307e360088
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-01-13 14:51:00 -0500

    Add a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,8 +3,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    
-    username = sys.argv[1]
+    user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
@@ -12,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
+        user=user,
         passwd=password,
         db=database
     )
