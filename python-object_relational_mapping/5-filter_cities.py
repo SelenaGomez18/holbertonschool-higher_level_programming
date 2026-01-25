470357e9cd78e454450e2114aff6573032bff95f
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-01-25 16:59:27 -0500

    Add a script that Lists all states from the database hbtn_0e_0_usa that match the state name provided as argument.

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
-import sys
+"""
+Lists all cities of a given state from the database hbtn_0e_4_usa
+"""
+
 import MySQLdb
+import sys
+
 
 def main():
-    if len(sys.argv) != 5:
-        return
-    user, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=user, passwd=password, db=db_name)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
     cursor = db.cursor()
-    query = ("SELECT cities.name FROM cities "
-             "JOIN states ON cities.state_id = states.id "
-             "WHERE states.name = %s "
-             "ORDER BY cities.id ASC")
+
+    query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+
     cursor.execute(query, (state_name,))
-    rows = cursor.fetchall()
-    cities = [row[0] for row in rows]
-    print(", ".join(cities))
+    results = cursor.fetchall()
+
+    print(", ".join(city[0] for city in results))
+
     cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     main()
