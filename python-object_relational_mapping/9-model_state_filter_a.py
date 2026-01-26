a4fed2a898775d551c25d80edaae5447cdadd13a
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-01-25 19:31:41 -0500

    Add a script that Lists all states from the database hbtn_0e_0_usa that match the state name provided as argument.

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Lists all State objects that contain the letter 'a'"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Create engine
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query states containing the letter 'a'
+    states = (
+        session.query(State)
+        .filter(State.name.like('%a%'))
+        .order_by(State.id)
+        .all()
+    )
+
+    # Display results
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
