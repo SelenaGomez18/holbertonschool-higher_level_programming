090ce7d18e51d8643d3f625d2d5ef3b3a87844dd
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-01-25 19:17:11 -0500

    Add a script that Lists all states from the database hbtn_0e_0_usa that match the state name provided as argument.

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""
-Defines the State class and Base instance
-"""
-
+"""Defines the State class and Base for SQLAlchemy ORM"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create the Base instance
 Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = "states"
+    """State class mapped to the states table"""
+    __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
